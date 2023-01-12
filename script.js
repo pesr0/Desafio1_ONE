6b5fb989801617fe9ccb5c8d36a680c0c4c94891
pedroT <pedrotmartin00@hotmail.com>
2023-01-11 22:38:22 -0300

    codigo passado

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 function codificar(){
-    var texto = document.querySelector('#caixaentrada').value
+    var texto = document.querySelector('#input_box').value
     var x=0;
     
+    function hasAccent(string) {
+        return /[áéíóúàèìòùãẽĩõũâêîôû]/i.test(string);
+    }
+
     for(var i=0; i<texto.length; i++){
-        if(texto[i]==texto[i].toUpperCase() && texto[i]!=' ' && texto[i]!='\n' && isNaN(texto[i]) ) x++;
+        if( (texto[i]==texto[i].toUpperCase() || hasAccent(texto)) && texto[i]!=' ' && texto[i]!='\n' && isNaN(texto[i]) ) x++;
     }
 
     if (x!=0) alert('Apenas letras minúsculas');
@@ -14,7 +18,7 @@ function codificar(){
         texto=texto.replaceAll('a',  'ai');
         texto=texto.replaceAll('u', 'ufat');
         texto=texto.replaceAll('o', 'ober');
-        document.querySelector('#caixasaida').value=texto;
+        document.querySelector('#output_box').value=texto;
     }
 
     
@@ -22,19 +26,19 @@ function codificar(){
 
 
 function decodificar(){
-    var texto = document.querySelector('#caixaentrada').value;
+    var texto = document.querySelector('#input_box').value;
     texto=texto.replaceAll('enter', 'e');
     texto=texto.replaceAll('imes', 'i');
     texto=texto.replaceAll('ai', 'a');
     texto=texto.replaceAll('ufat', 'u');
     texto=texto.replaceAll('ober', 'o');
-    document.querySelector('#caixasaida').value=texto;
+    document.querySelector('#output_box').value=texto;
 };
 
 
 function copy(){
-    var texto = document.querySelector('#caixasaida');
+    var texto = document.querySelector('#output_box');
     texto.select();
     document.execCommand('copy');
-    document.querySelector('#caixaentrada').value='';
+    document.querySelector('#input_box').value='';
 };
